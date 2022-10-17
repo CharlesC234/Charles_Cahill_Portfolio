ae9bf614d2d6f24e697b9dbe81ea304f072233f9
CharlesC234 <99014706+CharlesC234@users.noreply.github.com>
2022-10-17 13:46:20 -0400

    Add files via upload

---
+++ OOP_Java/1.04_Projects/StudentInfoCard.java
@@ -0,0 +1,55 @@
+
+/**
+ * Write a description of class StudentInfoCard here.
+ *Program for printing out student intfo card
+ * @author (Charles Cahill)
+ * @version (8/22/2020)
+ */
+public class StudentInfoCard
+{
+   public static void main(String [ ] args)
+   {
+       //Banner
+       System.out.println("***************************************************************************************************************************************************************");
+       System.out.println("*                                                                                                                                                             *");
+       System.out.println(" ");
+       System.out.println("*                                                         Student Contact Card                                                                                *");
+       System.out.println(" ");
+       System.out.println("*                                                                                                                                                             *");
+       System.out.println("***************************************************************************************************************************************************************");
+       System.out.println(" ");
+       System.out.print("Name: Charles Cahill   ");
+       System.out.println("Birthday: 01/29/2003   ");
+       System.out.println("Age: 17");
+       System.out.println(" ");
+       
+       System.out.print("My school: Marjory Stoneman Douglas High School   ");
+       System.out.print("My grade level: 12th/ Senior   ");
+       System.out.println("The city I live in: Coral Springs");
+       System.out.println(" ");
+      
+      
+       System.out.print("Home phone number: 954-993-5321   ");
+       System.out.println("My cell phone: 954-861-7198   ");
+       System.out.print("My email adress: Cahilcharles123@gmail.com   ");
+       System.out.println("Usually availible between the hours of 3-9 P.M.");
+       System.out.println(" ");
+       
+       
+       System.out.print("My dad's cell phone number: 954-297-4531    ");
+       System.out.println("My dad's email adress: JJccc99@att.net");
+       System.out.println(" ");
+       
+      
+       System.out.print("Current math course: Pre Calculus Honors   ");
+       System.out.println("Previous computer programming experience:"); 
+       System.out.println("Arduino IDE, programming microcontrollers in a version of C++");
+       System.out.println(" ");
+       
+       System.out.print("I am taking APCSA because I want to try to learn how to program ");
+       System.out.println("in Java, I also think that I will find this class interesting ");
+       
+       
+       
+    }
+}
